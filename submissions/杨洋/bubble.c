8c940a38759180a44377b3468100c701d5245104
doublesheep-xdu <2764957688@qq.com>
2024-09-12 23:23:32 +0800

    Update bubble.c

--- submissions/杨洋/bubble.c
+++ submissions/杨洋/bubble.c
@@ -1,28 +1,25 @@
-#include "bits/stdc++.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "algorithm"
 using namespace std;
 
-int bubble(int b)
-{
-        int a[b];
-        for (int i = 0; i < b; i++)
-                scanf("%d", &a[i]);
-        for (int i = 0; i < b; i++)
-        {
-                for (int j = i; j < b; j++)
-                {
-                        if (a[i] > a[j])
-                                swap(a[i], a[j]);
-                }
-        }
-        for (int i = 0; i < b; i++)
-                printf("%d ", a[i]);
-        return 0;
-}
 int main()
 {
         int n;
         printf("please  input the number of the numbers\n");
         scanf("%d", &n);
-        bubble(n);
+        int a[n];
+        for (int i = 0; i < n; i++)
+                scanf("%d", &a[i]);
+        for (int i = 0; i < n; i++)
+        {
+                for (int j = i; j < n; j++)
+                {
+                        if (a[i] > a[j])
+                                swap(a[i], a[j]);
+                }
+        }
+        for (int i = 0; i < n; i++)
+                printf("%d ", a[i]);
         return 0;
 }
